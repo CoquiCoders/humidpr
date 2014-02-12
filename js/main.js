4c769212a6f50da40602eae8bac560b4d4207fbe
Maksim Pecherskiy <max@maksimize.com>
2014-02-12 14:52:39 -0400

    Updating for forecast.io requests.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,52 @@
 
 function setUpTangle () {
 
+    var getWeather = function(lattitude, longitude, dateTime) {
+      if (!dateTime || dateTime == null) {
+        dateTime = Math.round(+new Date()/1000)
+      }
+      var url = 'http://api.forecast.io/forecast/a5e1c0cd83868863d7fe8b7a72375e3b/' + lattitude + ',' + longitude + ',' + dateTime;
+      //var url = 'http://api.forecast.io/forecast/a5e1c0cd83868863d7fe8b7a72375e3b/18.4500,66.0667,2014-02-12T20:00:00-0400';
+      return $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        contentType: "application/json",
+        dataType: 'jsonp',
+        //success: function(json) {
+        //  console.log(json);
+        //  tangle.weatherData = json;
+        //},
+      });
+    }
     var element = document.getElementById("scenario");
 
     var tangle = new Tangle(element, {
         initialize: function () {
-            this.acres = 3.5;
-            this.spaces_per_acre = 100; // units: car parking spaces
-
-            this.parking_demand = 25; // max number of spaces needed. Based on observation.
-            // editable (see update function):
-            this.parking_coverage = 100; // % of site covered with parking
+            this.lattitude = '18.4500';
+            this.longitude = '66.0667';
         },
         update: function () {
-            this.parking_spaces = Math.floor((this.parking_coverage / 100) * this.acres * this.spaces_per_acre);
-            this.empty_spaces = this.parking_spaces - this.parking_demand;
+            console.log('update');
+            if (!this.currentWeather) {
+                getWeather(this.lattitude, this.longitude).done(function(jqXHR, textStatus, errorThrow) {
+                   console.log('promise');
+                   console.log(jqXHR);
+                   console.log(textStatus);
+                   console.log(errorThrow);
+                   if (textStatus != 'success') {
+                       // do something.
+                   }
+                   this.currentWeather = jqXHR.currently;
+                   var width = $("#img-slider .right img").width();
+                   var left = (this.currentWeather.humidity * width);
+                   $("#img-slider .right").css("clip", "rect(0, 1000px, 1000px, " + left + "px)"); /* top, right, bottom, left */
 
-
-            if (this.empty_spaces < 0) {
-                this.banana_merchants = Math.abs(this.empty_spaces);
-                this.angry_mjs = "few";     
-                console.log("But there may be as many as " + Math.abs(this.empty_spaces) + " angry neighbors.");
-                this.empty_spaces = 0;
+                });
             }
-
-            else
-            {
-              this.banana_merchants = "no";
-               this.angry_mjs = this.parking_spaces;
-
-            }
-
-            /* Update the percentage of each image shown based on the parking_coverage % chosen by user. */
-            /*
-                | ----------------------------- 100 % ------------------------------- |
-                | ------- parking_coverage% ---   |   ---------  Park Img  ---------- |
-                | ------------------------------------------------------------------- |
-            */
-            var width = $("#img-slider .right img").width();
-            var left = (this.parking_coverage/100 * width);
-            $("#img-slider .right").css("clip", "rect(0, 1000px, 1000px, " + left + "px)"); /* top, right, bottom, left */
 
         }
+
     });
 }
+
