baf920d169dcae317b86968b90967369a2b1bf29
Maksim Pecherskiy <max@maksimize.com>
2014-02-12 17:51:03 -0400

    Various css and copy cleanups.

--- js/main.js
+++ js/main.js
@@ -1,52 +1,53 @@
 
-function setUpTangle () {
+var humidifier = {
+      lattitude: '18.4500',
 
-    var getWeather = function(lattitude, longitude, dateTime) {
-      if (!dateTime || dateTime == null) {
-        dateTime = Math.round(+new Date()/1000)
-      }
-      var url = 'http://api.forecast.io/forecast/a5e1c0cd83868863d7fe8b7a72375e3b/' + lattitude + ',' + longitude + ',' + dateTime;
-      //var url = 'http://api.forecast.io/forecast/a5e1c0cd83868863d7fe8b7a72375e3b/18.4500,66.0667,2014-02-12T20:00:00-0400';
-      return $.ajax({
-        type: 'GET',
-        url: url,
-        async: false,
-        contentType: "application/json",
-        dataType: 'jsonp',
-        //success: function(json) {
-        //  console.log(json);
-        //  tangle.weatherData = json;
-        //},
-      });
-    }
-    var element = document.getElementById("scenario");
+      longitude: '66.0667',
 
-    var tangle = new Tangle(element, {
-        initialize: function () {
-            this.lattitude = '18.4500';
-            this.longitude = '66.0667';
-        },
-        update: function () {
-            console.log('update');
-            if (!this.currentWeather) {
-                getWeather(this.lattitude, this.longitude).done(function(jqXHR, textStatus, errorThrow) {
-                   console.log('promise');
-                   console.log(jqXHR);
-                   console.log(textStatus);
-                   console.log(errorThrow);
-                   if (textStatus != 'success') {
-                       // do something.
-                   }
-                   this.currentWeather = jqXHR.currently;
-                   var width = $("#img-slider .right img").width();
-                   var left = (this.currentWeather.humidity * width);
-                   $("#img-slider .right").css("clip", "rect(0, 1000px, 1000px, " + left + "px)"); /* top, right, bottom, left */
+      getWeather: function(lattitude, longitude, dateTime) {
+          if (!dateTime || dateTime == null) {
+            dateTime = Math.round(+new Date()/1000)
+          }
+          var url = 'http://api.forecast.io/forecast/a5e1c0cd83868863d7fe8b7a72375e3b/' + lattitude + ',' + longitude + ',' + dateTime;
+          return $.ajax({
+            type: 'GET',
+            url: url,
+            async: false,
+            contentType: "application/json",
+            dataType: 'jsonp',
+          });
+      },
 
-                });
-            }
-
+      getImageForHumidity: function() {
+        if (this.humidity <= 0.7) {
+            return 'park-export.jpg';
         }
+        else {
+            return 'parking-export.jpg';
+        }
+      },
 
-    });
+      humidify: function () {
+          if (!this.currentWeather) {
+              var self = this;
+              this.getWeather(this.lattitude, this.longitude).done(function(jqXHR, textStatus, errorThrow) {
+                  console.log('promise');
+                  console.log(jqXHR);
+                  console.log(textStatus);
+                  console.log(errorThrow);
+                  if (textStatus != 'success') {
+                        // do something.
+                  }
+                  humidifier.currentWeather = jqXHR.currently;
+                  var image = humidifier.getImageForHumidity();
+                  $('.image-container img').attr('src', 'img/' + image);
+
+           //       var height = $("#img-slider .right img").height();
+       //          var offset = (this.currentWeather.humidity * height);
+         //         $("#img-slider .right").css("clip", "rect(0, 1000px, " + offset + "px, 0px)"); /* top, right, bottom, left */
+
+              });
+          }
+      }
 }
 
